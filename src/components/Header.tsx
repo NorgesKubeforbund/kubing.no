c043638187bab2de87d067908f1099150cf920a0
Tore Andre Orheim <toreorheim@gmail.com>
2022-10-18 14:22:44 +0200

    mapping header buttons

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -2,6 +2,18 @@ import './Header.css';
 import image from '../media/NKF_Logo_trans.png';
 
 export const NavBar = () => {
+
+  const pages = [
+    {name: 'Hjem', path: '/'}, 
+    {name: 'Konkurranser', path: '/Konkurranser'}, 
+    {name: 'Butikker', path: '/Butikker'}, 
+    {name: 'Guider', path: '/Guider'}, 
+    {name: 'Om Oss', path: '/OmOss'}, 
+    {name: 'Norske Rekorder', path: '/Rekorder'}, 
+    {name: 'Eksterne Ressurser', path: '/EksterneRessurser'}, 
+    {name: 'Lokale Arrangement', path: '/LokaleArrangement'},
+  ]
+
   return (
   <header className="App-header">
     <div>
@@ -10,30 +22,21 @@ export const NavBar = () => {
     </div>
     <a>Norges Kubeforbund</a>
       <div className="NavBar">
-        <button onClick={() => {
-          window.location.href = "./"
-          }} className="MenuLinks active">Hjem</button>
-        <button onClick={() => {
-          window.location.href = "./Konkurranser"
-          }} className="MenuLinks">Konkurranser</button>
-        <button onClick={() => {
-          window.location.href = "./Butikker"
-          }} className="MenuLinks">Butikker</button>       
-          <button onClick={() => {
-          window.location.href = "./Guider"
-          }} className="MenuLinks">Guider</button>                       
-          <button onClick={() => {
-          window.location.href = "./OmOss"
-          }} className="MenuLinks">Om oss</button>         
-          <button onClick={() => {
-          window.location.href = "./Rekorder"
-          }} className="MenuLinks">Norske rekorder</button>
-          <button onClick={() => {
-          window.location.href = "./EksterneRessurser"
-          }} className="MenuLinks">Eksterne ressurser</button>
-          <button onClick={() => {
-          window.location.href = "./LokaleArrangement"
-          }} className="MenuLinks">Lokale arrangement</button>
+        {pages.map((pg) => {
+          if (pg['path'] == window.location.pathname) {
+            return (
+              <button key={pg['name']} onClick={() => {
+                window.location.href = pg['path']
+              }} className="MenuLinks active">{pg['name']}</button>
+            )
+          } else {
+            return (
+              <button onClick={() => {
+                window.location.href = pg['path']
+              }} className="MenuLinks">{pg['name']}</button>
+            )
+          }
+        })}
         <div className="DropDown">
           <div className="IconBar"></div>
           <div className="IconBar"></div>
