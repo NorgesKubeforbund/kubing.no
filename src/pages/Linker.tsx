2e5e62f4e4b998d727c8970819948fe68da63663
Tore Orheim <toreorheim@gmail.com>
2022-10-28 11:20:18 +0200

    Initial

--- src/pages/Linker.tsx
+++ src/pages/Linker.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 import { NavBar } from '../components/Header';
 import './Linker.css';
 
-function EksterneRessurser() {
+function Linker() {
     return (
         <div className="Eksterne Ressurser">
             <NavBar/>
@@ -13,4 +13,4 @@ function EksterneRessurser() {
     );
 }
 
-export default EksterneRessurser;
+export default Linker;
