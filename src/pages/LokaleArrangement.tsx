ab407bbdb008025de85de3362097f153c65d2732
Einar Martin Sandvik <einarmsa@stud.ntnu.no>
2022-10-28 11:45:23 +0200

    modify links and small changes

--- src/pages/LokaleArrangement.tsx
+++ src/pages/LokaleArrangement.tsx
@@ -12,26 +12,26 @@ function LokaleArrangement() {
                     Ønsker du å finne andre kubere å løse sammen med? 
                     Det finnes flere lokale arragement rundt omkring i landet som møtes gjevnlig for å kube sammen.
                     Mange av de størtse byene har tilbud, men du kan også spørre 
-                    på facebook-siden <a href='https://www.facebook.com/groups/NorskeSpeedcubers' target='_blank'>Norske SpeedCubers</a> om det er noen kubere i nærheten av deg.
+                    på facebook-siden <a href='https://www.facebook.com/groups/NorskeSpeedcubers' target='_blank' rel='noopener'>Norske SpeedCubers</a> om det er noen kubere i nærheten av deg.
                     Kanskje du kan bidra til å skape et tilbud for ditt lokalmiljø?
                 </div>
                 
                 <div className='LokaltArrangement'>
                     <h2>Oslo-viken</h2>
-                    <a href='https://www.facebook.com/groups/529099402302831/' target='_blank'>Cube Drammen</a> har tilbud i Drammen.<br></br>
-                    <a href='https://www.facebook.com/groups/2430272227122282/' target='_blank'>Kubing i Bærum</a> har tilbud i Bærum.<br></br>
+                    <a href='https://www.facebook.com/groups/529099402302831/' target='_blank' rel='noopener'>Cube Drammen</a> har tilbud i Drammen.<br></br>
+                    <a href='https://www.facebook.com/groups/2430272227122282/' target='_blank' rel='noopener'>Kubing i Bærum</a> har tilbud i Bærum.<br></br>
                     <a href='https://spond.com/landing/group/TSRZF' target='_blank'>Romerike SpeedCubers</a> har tilbud i Lillestrøm.
                 </div>
                 <div className='LokaltArrangement'>
                     <h2>Rogaland</h2>
-                    <a href='https://www.facebook.com/groups/1658988921015490' target='_blank'>Sandnes SpeedCubers</a> har tilbud i sannes.
+                    <a href='https://www.facebook.com/groups/1658988921015490' target='_blank' rel='noopener'>Sandnes SpeedCubers</a> har tilbud i sannes.
                 </div>
                 <div className='LokaltArrangement'>
                     <h2>Trøndelag</h2>
                     I trondheim er det to forskjellige tilbud:<br></br>
                     
-                    <a href='https://www.facebook.com/groups/1345849928919723' target='_blank'>Flatåsen Kubeklubb</a> har tilbud.<br></br>
-                    NTNUI-Speedcubing har tilbud.
+                    <a href='https://www.facebook.com/groups/1345849928919723' target='_blank' rel='noopener'>Flatåsen Kubeklubb</a> har tilbud.<br></br>
+                    <a href='https://www.facebook.com/NTNUISpeedcubing' target='_blank' rel='noopener'>NTNUI Speedcubing</a> har tilbud.
                 </div>
 
 
