e326d28e4b91a1f97292443e4222eaacdda1b1a9
Tore Orheim <toreorheim@gmail.com>
2022-12-12 11:53:39 +0100

    populated modal

--- src/pages/Konkurranser.tsx
+++ src/pages/Konkurranser.tsx
@@ -9,6 +9,7 @@ function Konkurranser() {
 
   const [loading, setLoading] = useState<boolean>(false);
   const [compData, setCompData] = useState<apiResponse[]>([]);
+  const [arrData, setArrData] = useState<string[][]>([]);
   const [modalVisible, setModalVisible] = useState<boolean>(false);
 
   type apiResponse = 
@@ -80,12 +81,24 @@ function Konkurranser() {
     class: string
   }
 
-  const getCompData = async(): Promise<apiResponse> => {
+  const getCompData = async(): Promise<void> => {
     setLoading(true);
-    const response = await axios.get(`${process.env.REACT_APP_KONKURRANSE_KEY}`);
-    setCompData(response.data);
+    try {
+      await axios.get(`${process.env.REACT_APP_KONKURRANSE_KEY}`)
+        .then(response => setCompData(response.data));
+      await axios.get(`${process.env.REACT_APP_ARRANGEREKONKURRASER_KEY}`)
+        .then(response => setArrData(response.data.values));
+      
+    } catch (error) {
+      let message: string;
+      if (error instanceof Error) {
+        message = error.message;
+      } else {
+        message = String(error);
+      }
+      alert(message);
+    }
     setLoading(false);
-    return await response.data;
   }
 
   useEffect(() => {
@@ -98,6 +111,7 @@ function Konkurranser() {
         isOpen={modalVisible}
         onRequestClose={() => setModalVisible(!modalVisible)}
         className="modalContainer"
+        ariaHideApp={false}
       >
         <button
           className="modalCloseButton"
@@ -105,9 +119,16 @@ function Konkurranser() {
         Lukk vindu
         </button>
         <div className='modalContent'>
-          <p>
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur placerat sapien at justo porttitor imperdiet. Nunc in quam fermentum, consequat ligula et, rhoncus diam. Aliquam et ultricies lorem, non pellentesque lacus. Cras arcu metus, eleifend ut vestibulum nec, dignissim in dolor. Aenean id euismod odio. Nulla tempus lacus non lacus suscipit, vel scelerisque erat malesuada. Suspendisse a mattis mauris. Sed nec massa sed tellus pharetra consequat sed a leo. Quisque viverra sapien lectus, vehicula interdum augue vulputate at.
-          </p>
+          {arrData.map((el: string[]) => {
+            return (
+              <div key={el[2]+'Div'}>
+                <p key={el[2]+'Text'}>{el[0]}</p>
+                {
+                  el[1] === "" ? <p>{el[2]}</p> : <a key={el[2]+'Link'} href={el[1]}>{el[2]}</a>
+                }
+              </div>
+            )
+          })}
         </div>
       </Modal>
     )
