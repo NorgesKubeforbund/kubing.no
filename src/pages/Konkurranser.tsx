f68aa9e1d85bcb7022ecd0babbf929b5ba45053a
Lars Johan Folde <ljfolde@gmail.com>
2024-04-01 03:02:13 +0200

    Aligned list of Delegates into columns

--- src/pages/Konkurranser.tsx
+++ src/pages/Konkurranser.tsx
@@ -121,41 +121,44 @@ function Konkurranser() {
         WCA har mange ressurser som kan hjelpe til med å arrangere konkurranser. Disse kan du finne via lenken under.
         <br></br>
         <br></br>
-        <ExternalLink href="https://www.worldcubeassociation.org/organizer-guidelines">Organizer-Guidelines from WCA</ExternalLink>
-        <br></br>
-        <br></br>
-        Etter at du har skaffet deg et overblikk over hvordan en arrangerer en konkurranse og laget en plan for hvordan du ønsker å
-        gjennomføre din konkurranse, må du ta kontakt med en delegat. Alle norske delegater er listet nedenfor.
-        <h2>Norske delegater</h2>
-        <ExternalLink href="mailto:ubredland@worldcubeassociation.org,vklungre@worldcubeassociation.org,ironmeadow@gmail.com">Delegater i Oslo - Kontakt</ExternalLink>
-        <br></br>
-        <br></br>
-        Ulrik Bredland - <ExternalLink href="https://www.worldcubeassociation.org/persons/2012BRED01">WCA-Profil</ExternalLink>
-        <br></br>
-        Vidar Klungre - <ExternalLink href="https://www.worldcubeassociation.org/persons/2008KLUN01">WCA-Profil</ExternalLink>
-        <br></br>
-        Jakob Jernsletten - <ExternalLink href="https://www.worldcubeassociation.org/persons/2018JERN01">WCA-Profil</ExternalLink>
-        <br></br>
-        <br></br>
-        <hr></hr>
-        <br></br>
-        <ExternalLink href="mailto:jbruun@worldcubeassociation.org,lfolde@worldcubeassociation.org">Delegater i Trondheim - Kontakt</ExternalLink>
-        <br></br>
-        <br></br>
-        Lars Johan Folde - <ExternalLink href="https://www.worldcubeassociation.org/persons/2018FOLD01">WCA-Profil</ExternalLink>
-        <br></br>
-        Jacob Oliver Bruun - <ExternalLink href="https://www.worldcubeassociation.org/persons/2018BRUU01">WCA-Profil</ExternalLink>
-        <br></br>
-        <br></br>
-        <hr></hr>
-        <br></br>
-        <ExternalLink href="mailto:ejohnsen@worldcubeassociation.org">Delegater i Nord Dakota, USA - Kontakt</ExternalLink>
-        <br></br>
-        <br></br>
-        Elmer Alexander Johnsen - <ExternalLink href="https://www.worldcubeassociation.org/persons/2018JOHN03">WCA-Profil</ExternalLink>
-        <br></br>
-        <br></br>
-        <hr></hr>
+          <ExternalLink href="https://www.worldcubeassociation.org/organizer-guidelines">Organizer-Guidelines from WCA</ExternalLink>
+          <br></br>
+          <br></br>
+          Etter at du har skaffet deg et overblikk over hvordan en arrangerer en konkurranse og laget en plan for hvordan du ønsker å
+          gjennomføre din konkurranse, må du ta kontakt med en delegat. Alle norske delegater er listet nedenfor.
+          <br></br>
+          <br></br>
+          <h2 className='delegateHeader'>Norske Delegater</h2>
+        <div className='delegateColumn'>
+        <strong>
+          <ExternalLink className='delegateContact' href="mailto:ubredland@worldcubeassociation.org,vklungre@worldcubeassociation.org,ironmeadow@gmail.com">Delegater i Oslo - Kontakt</ExternalLink>
+        </strong>
+          <br></br>
+          <br></br>
+          <ExternalLink className='delegate' href="https://www.worldcubeassociation.org/persons/2012BRED01">Ulrik Bredland</ExternalLink>
+          <br></br>
+          <ExternalLink className='delegate' href="https://www.worldcubeassociation.org/persons/2008KLUN01">Vidar Klungre</ExternalLink>
+          <br></br>
+          <ExternalLink className='delegate' href="https://www.worldcubeassociation.org/persons/2018JERN01">Jakob Jernsletten</ExternalLink>
+        </div>
+        <div className='delegateColumn'>
+        <strong>
+          <ExternalLink className='delegateContact' href="mailto:jbruun@worldcubeassociation.org,lfolde@worldcubeassociation.org">Delegater i Trondheim - Kontakt</ExternalLink>
+        </strong>
+          <br></br>
+          <br></br>
+          <ExternalLink className='delegate' href="https://www.worldcubeassociation.org/persons/2018FOLD01">Lars Johan Folde</ExternalLink>
+          <br></br>
+          <ExternalLink className='delegate' href="https://www.worldcubeassociation.org/persons/2018BRUU01">Jacob Oliver Bruun</ExternalLink>
+        </div>
+        <div className='delegateColumn'>
+        <strong>
+          <ExternalLink className='delegateContact' href="mailto:ejohnsen@worldcubeassociation.org">Delegater i Nord Dakota, USA - Kontakt</ExternalLink>
+        </strong>
+          <br></br>
+          <br></br>
+          <ExternalLink className='delegate' href="https://www.worldcubeassociation.org/persons/2018JOHN03">Elmer Alexander Johnsen</ExternalLink>
+        </div>
       </div>
     );
   }
