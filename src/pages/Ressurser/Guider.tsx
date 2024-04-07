2838455b3fc43c1590bf3a3dcb40b2858a3d19ae
Lars Johan Folde <ljfolde@gmail.com>
2024-04-07 18:17:33 +0200

    Removed unused imports

--- src/pages/Ressurser/Guider.tsx
+++ src/pages/Ressurser/Guider.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react';
+import React from 'react';
 import './Guider.css';
 import Nybegynnermetode from '../../media/3x3-Begynnermetode.pdf';
 import ExternalLink from '../../components/ExternalLink';
