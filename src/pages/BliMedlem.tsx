ee7ad83b4c92325910e777c9cfc973287c3482f3
Lars Johan Folde <ljfolde@gmail.com>
2025-02-05 20:22:15 +0100

    Oppdatert Bli Medlem
    Oppdatert bli medlem for å reflektere den nye prosedyren. Dette kan bli sett på som en midlertidig løsning i påvente av et redesign av nettsiden

--- src/pages/BliMedlem.tsx
+++ src/pages/BliMedlem.tsx
@@ -10,14 +10,26 @@ function BliMedlem(): React.ReactElement<any, any> {
             Medlem får ofte rabatt på norske konkurranser.
             <br></br>
             <br></br>
-            For å bli medlem må du gjøre to ting:
+              For å bli medlem må du gjøre to ting:
+
             <br></br>
-            For de med Vipps: betal <span className="boldText">100 kroner</span> til 24441 og merk meldingen med navn.
             <br></br>
-            For de uten Vipps: overfør <span className="boldText">100 kroner</span> til kontonummer 1503.13.61831.
+            <div className="steg">
+            <span className="boldText">Steg 1</span>
             <br></br>
-            Deretter send en epost til medlem@kubing.no med <span className="boldText">fullt navn, e-post, telefonnummer, kjønn, adresse, og fødselsdato.</span>
+              For de med Vipps: betal 100 kroner til 24441 og merk meldingen med navn.
             <br></br>
+            <br></br>
+              For de uten Vipps: overfør 100 kroner til kontonummer 1503.13.61831.
+            </div>
+            <br></br>
+            <div className="steg">
+            <span className="boldText">Steg 2</span>
+
+            <br></br>
+              Fyll ut <a href="https://docs.google.com/forms/d/e/1FAIpQLSdj4MXBIQWHjsECp_gma4rRzp4MfDfDtohsfU-wDVW-dsY9qA/viewform?usp=header">registreringsskjemaet</a> via google, og last opp et skjermbilde av betaling.
+            <br></br>
+              </div>
             <br></br>
             Om du er usikker på om du allerede er medlem, kan du sende mail til <a href="mailto:medlem@kubing.no">medlem@kubing.no</a> og spørre.
         </div>
